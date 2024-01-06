2a7f4a34b1c21efd6584f45b69546e351ef02d7a
alexey-polyashov <polyashofff@yandex.ru>
2024-01-06 23:22:23 +0300

    crud api

---
+++ src/main/java/com/example/adrbook/config/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.example.adrbook.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    //jar does not start if this code is missing
+    @Bean
+    public DataSource getDataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:mysql://localhost:3306/mikron")
+                .username("root")
+                .password("")
+                .build();
+    }
+}
